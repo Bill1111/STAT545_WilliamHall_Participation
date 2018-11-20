879438774f7356a938c9db7b1303b8a9978f7902
William Hall <will.hall50@gmail.com>
2018-11-20 10:45:14 -0800

    Commit

--- Class20.Nov13/bcl/app.R
+++ Class20.Nov13/bcl/app.R
@@ -1,7 +1,10 @@
 
 library(shiny)
+library(tidyverse)
 
 bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+
+##Make the transition probabilities dependent on the transitions they pick. "Does your new program impact the following? ED visits, Hospital Stays, Residential Care, Transition to Death##
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -9,7 +12,13 @@ ui <- fluidPage(
   titlePanel("BC Liquor price app", 
              windowTitle = "BCL app"),
   sidebarLayout(
-    sidebarPanel("This text is in the sidebar."),
+    sidebarPanel("This text is in the sidebar.",
+                 sliderInput("priceInput", "Select your desired price range.",
+                             min = 0, max = 100, value = c(15, 30), pre="$"),
+                 radioButtons("typeInput", "Select Yo Bev Bruh",
+                              choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                              selected = "WINE")
+                 ),
     mainPanel("This text is in the main panel.",
               plotOutput("price_hist"),
               tableOutput("bcl_data"))
@@ -19,10 +28,21 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  observe(print(input$priceInput))
   
-  output$price_hist <- renderPlot(ggplot2::qplot(bcl$Price))
+  bcl_filtered <- reactive(bcl %>% 
+                filter(Price < input$priceInput[2],
+                Price > input$priceInput[1],
+                Type == input$typeInput)
+)
+        
+  output$price_hist <- renderPlot(bcl_filtered() %>% 
+                                    ggplot(aes(Price)) +
+                                    geom_histogram()
+  )
   
-  output$bcl_data <- renderTable(bcl)
+  output$bcl_data <- renderTable(bcl_filtered()
+  )
 }
 
 # Run the application 
